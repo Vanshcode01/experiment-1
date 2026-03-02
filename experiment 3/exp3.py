ca84fef25ee49ffe4d591391ffdd4c9a52fa0a0f
Vanshcode01 <vansh.28936@stu.upes.ac.in>
2026-03-02 10:51:48 +0530

    Add files via upload

---
+++ experiment 3/exp3.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("Greatest is", a)
+elif b > a:
+    print("Greatest is", b)
+else:
+    print("numbers are equal")
